4252709cf0765ae9f30a1d539c2926552fcabf2f
da_saint <50449180+dasaint001@users.noreply.github.com>
2020-06-05 21:07:52 +0100

    Update da_saint-HNG-00657.py

--- scripts/da_saint-HNG-00657.py
+++ scripts/da_saint-HNG-00657.py
@@ -2,4 +2,9 @@ name = "Aborisade Elijah"
 hngi7Id = "HNG-00657"
 email = "aborisadeelijah@gmail.com"
 language = "Python"
-print(f"Hello World, this is {name} with HNGi7 ID {hngi7Id} and email {email} using {language} for stage 2 task")
+
+def mine_result(name, hngi7Id, email, language):
+    return  print('Hello World, this is {name} with HNGi7 ID {hngID} and email {email} using {language} for stage 2 task'.format(name=name, hngID=hngi7Id, email=email, language=language))
+
+
+mine_result(fullName, hngi7Id, email, language)
