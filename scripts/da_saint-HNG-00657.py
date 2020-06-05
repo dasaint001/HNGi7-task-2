af73cb1ed54664d9802b8962774fee6814aa4208
da_saint <50449180+dasaint001@users.noreply.github.com>
2020-06-05 21:56:27 +0100

    Update da_saint-HNG-00657.py

--- scripts/da_saint-HNG-00657.py
+++ scripts/da_saint-HNG-00657.py
@@ -1,5 +1,4 @@
-name = "Aborisade Elijah"
-hngi7Id = "HNG-00657"
-email = "aborisadeelijah@gmail.com"
-language = "Python"
-print(f"Hello World, this is {name} with HNGi7 ID {hngi7Id} and email {email} using {language} for stage 2 task")
+hngi7Id = "HNG-00657"
+email = "aborisadeelijah@gmail.com"
+language = "Python"
+print(f"Hello World, this is {name} with HNGi7 ID {hngi7Id} and email {email} using {language} for stage 2 task")
